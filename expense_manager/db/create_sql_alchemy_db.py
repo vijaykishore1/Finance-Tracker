2cfb71f399ad800e791fda65ba0d8674a75245b5
vijaykishore1 <vijaykishore1998@gmail.com>
2022-09-12 16:25:30 +0530

    deleted pycache

--- expense_manager/db/create_sql_alchemy_db.py
+++
@@ -1,3 +0,0 @@
-from db_schema_sql_alchemy import db
-
-db.create_all()
--- expense_manager/pandasss/reading as tables.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from expense_manager.db.db_utils import DbUtils
-from expense_manager.constants.table_names import LOGIN_TABLE, EXPENSES_TABLE, EXPENSES_CATEGORY_TABLE, \
-    INCOME_CATEGORY_TABLE, INVESTMENTS_CATEGORY_TABLE, INVESTMENTS_TABLE, INCOME_TABLE, BANK_ACCOUNT_TABLE, USER_TABLE
-
-with DbUtils() as utils_obj:
-    sql_data = pd.read_sql(f"select * from {LOGIN_TABLE},{EXPENSES_TABLE},{INVESTMENTS_TABLE},{INCOME_TABLE},{BANK_ACCOUNT_TABLE},{USER_TABLE} where username = 'priyav' and id = 1",utils_obj.conn)
-print(sql_data)
